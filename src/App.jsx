ea6d557969a07de24afe6648b779a9f50a0a226f
Shubham Mishra <shubhammishra10101998@gmail.com>
2025-01-27 18:44:29 +0545

    basic todo app

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { TodoProvider } from './context';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
+  };
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) => (prevTodo.id === todo.id ? todo : prevTodo))
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((prevTodo) => prevTodo.id !== id));
+  };
+
+  const toggleComplete = (id) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) =>
+        prevTodo.id === id
+          ? { ...prevTodo, completed: !prevTodo.completed }
+          : prevTodo
+      )
+    );
+  };
+
+  return (
+    <TodoProvider
+      value={{ addTodo, updateTodo, deleteTodo, todos, toggleComplete }}
+    >
+      <h1>Context API with Local Storage.</h1>
+    </TodoProvider>
+  );
+}
+
+export default App;
